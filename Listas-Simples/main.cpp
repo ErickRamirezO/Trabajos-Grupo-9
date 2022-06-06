5eb4873d3e7addbd75418eb9d61617d6a1422485
ErickRamirezO <87491977+ErickRamirezO@users.noreply.github.com>
2022-06-06 08:22:21 -0500

    Listas simples con funciones insertar, eliminar, etc

--- Listas-Simples/main.cpp
+++ Listas-Simples/main.cpp
@@ -1,11 +1,21 @@
+//UNIVERSIDAD DE LAS FUERZAS ARMADAS "ESPE"
+//Implementación de funciones para listas simples
+//Insertar por cola, cabeza, eliminar por la cola, cabeza.
+//Busqueda, imprimir por cola, cabeza.
+//FECHA DE CREACION 28/05/2022
+//FECHA DE MODIFICACION 30/05/2022
+//GRUPO #9 
+//INTEGRANTES: Ramirez Ortiz Erick Patricio 
+//             Yanez Gutierrez Michelle Estefania 
+// GITHUB DEL GRUPO:  
+// https://github.com/ErickRamirezO/Trabajos-Grupo-9
+
 #include <iostream>
 #include "Lista.h"
 // #include "Nodo.h"
 #include "menu.cpp"
 using namespace std;
 
-//Universidad de las Fuerzas Armadas ESPE
-//NOMBRE: Erick Ramirez
 
 int main() {
 	Lista lista;
